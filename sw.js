5c455d2cba9fc0ca90ad48b05e3e5f145dd0d6e4
Anthony Willey <adjwilley@gmail.com>
2026-09-18 12:49:07 -0700

    Tromell: headline as two centred columns; simpler Seymour/Ouroboroid notes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Stale-while-revalidate: the app opens instantly and works with no signal,
    but a new deploy is picked up in the background and used next launch. */
-var CACHE = "elfball-v15";
+var CACHE = "elfball-v16";
 var ASSETS = ["./", "./index.html", "./tromell.html", "./manifest.webmanifest",
               "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 
