4cb89fe744f075789bf8b79124d0e0cdb794be8e
Anthony Willey <adjwilley@gmail.com>
2026-09-17 22:29:59 -0700

    This-turn panel to top; Undo/New turn alone top-right, Sound/New game small under the turn number

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Stale-while-revalidate: the app opens instantly and works with no signal,
    but a new deploy is picked up in the background and used next launch. */
-var CACHE = "elfball-v5";
+var CACHE = "elfball-v6";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
               "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 
